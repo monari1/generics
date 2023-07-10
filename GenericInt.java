97b95e6a4cdaa04f79b285af140733f4ce6e23ff
monari1 <francisnyasinga@gmail.com>
2023-07-10 22:55:39 +0300

    integer  generics

---
+++ GenericInt.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class GenericInt {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(5);
+        list1.add(1);
+        int s = list1.get(0);
+        System.out.println(s);
+    }
+}
