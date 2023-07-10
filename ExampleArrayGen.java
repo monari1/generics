4f9b902849b96daae38ea1023deacf186764bc2e
monari1 <francisnyasinga@gmail.com>
2023-07-10 22:52:59 +0300

    strings generics

---
+++ ExampleArrayGen.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ExampleArrayGen {
+    public static void main(String[] args) {
+        ArrayList<String> list1 = new ArrayList<>();
+        list1.add("red");
+        list1.add("white");
+        String s = list1.get(0);
+        System.out.println(s);
+    }
+}
