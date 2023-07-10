abc23f02b73391d4d1dd1f2afe031d8978888d41
monari1 <francisnyasinga@gmail.com>
2023-07-10 22:58:07 +0300

    double  generics

---
+++ genericDouble.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class genericDouble {
+    public static void main(String[] args) {
+        ArrayList<Double> list1 = new ArrayList<>();
+        list1.add(5.0);
+        list1.add(1.4);
+        double s = list1.get(0);
+        System.out.println(s);
+    }
+}
